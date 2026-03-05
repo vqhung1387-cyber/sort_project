cd6e98da5b120a4df6fa8627aeea46a8c4f17fc6
nqthinh2512 <quozthiz1200@gmail.com>
2026-03-05 21:20:33 +0700

    Update command.h

--- source/command.h
+++ source/command.h
@@ -17,6 +17,10 @@ long long perform_count_comp(char* sort_name, int arr[], int n);
 
 void output_param(char* param, char* sort_name, sortFunction sort_func, int n, int arr[]);
 
+void output_TWOsort(char* sort_name1, char* sort_name2, sortFunction sort_func1, sortFunction sort_func2, int n, int arr[]);
+
+void print_TWOsorting_algorithm(char* sort_name1, char* sort_name2);
+
 void do_command_1(int n, char* requirements[]);
 
 void do_command_2(int n, char* requirements[]);
